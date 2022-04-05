2494e3c98e6a0a18a96c841b2e6db6436e689a46
anandupender <anand.upender@gmail.com>
2022-04-05 00:02:16 -0700

    most of griffins feedback

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var callout = document.querySelector("#openToAll");
 var texts = ["COME ONE <BR/> COME ALL", "BRING A<BR/> FRIEND","OPEN TO <BR/> ALL","NO TO-GO<BR/>CUPS","SEE YOU<BR/>THERE","MAKE A<BR/>FRIEND","SMILES<BR/>ONLY","OPEN TO<BR/>COLLABS"];
 var currPos = 0;
 
-window.setInterval(changeText,2000);
+window.setInterval(changeText,1600);
 
 function changeText(){
     currPos++;
@@ -14,8 +14,10 @@ function changeText(){
 
 // Load video animation once video canplay
 const video = document.querySelector('.hero');
+const nextEvent = document.querySelector('#nextEvent');
 
 video.addEventListener('canplay', (event) => {
   console.log("video loaded");
-  video.style.animation = "reveal 1000ms ease-in-out forwards"
+  video.style.animation = "reveal 1000ms ease-in-out forwards";
+  nextEvent.style.animation = "moveDown 500ms ease-out 1400ms forwards, pulse 750ms linear 1600ms infinite alternate";
 });
